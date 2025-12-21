05bfccfc21063d5993f8dcedc5dd2c7eb2c5fa97
DTStormBoBo <duongtranbao3010@gmail.com>
2025-12-21 11:27:10 +0700

    Chỉnh sửa giao diện

--- CinemaS/Program.cs
+++ CinemaS/Program.cs
@@ -148,27 +148,27 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.Use(async (context, next) =>
-{
-    // chỉ xử lý request vào trang root "/"
-    if (context.Request.Path == "/")
-    {
-        var user = context.User;
+//app.Use(async (context, next) =>
+//{
+//    // chỉ xử lý request vào trang root "/"
+//    if (context.Request.Path == "/")
+//    {
+//        var user = context.User;
 
-        // đã đăng nhập + là Admin -> Admin/Index
-        if (user?.Identity?.IsAuthenticated == true && user.IsInRole("Admin"))
-        {
-            context.Response.Redirect("/Admin/Index");
-            return;
-        }
+//        // đã đăng nhập + là Admin -> Admin/Index
+//        if (user?.Identity?.IsAuthenticated == true && user.IsInRole("Admin"))
+//        {
+//            context.Response.Redirect("/Admin/Index");
+//            return;
+//        }
 
-        // còn lại -> Home/Index
-        context.Response.Redirect("/Home/Index");
-        return;
-    }
+//        // còn lại -> Home/Index
+//        context.Response.Redirect("/Home/Index");
+//        return;
+//    }
 
-    await next();
-});
+//    await next();
+//});
 
 app.MapStaticAssets();
 app.MapRazorPages();
